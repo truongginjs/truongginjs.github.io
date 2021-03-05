b32f46a1be58315acd7d71ddf6d83c7b65574a31
truongginjs <truongginjs@gmail.com>
2021-03-05 11:48:59 +0700

    fix timer

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const audioMeo = new Audio('./resource/meomeomeo.wav');
 const audioOhno = new Audio('./resource/ohnoshort.wav');
 
+const tranDucBo = $('.tran-duc-bo');
+
 function pauseAudio(audio){
     audio.pause()
     audio.currentTime = 0;
@@ -18,10 +20,13 @@ $(".present").mouseout(function () {
 });
 
 $(".present").click(async function (e) { 
+    tranDucBo.addClass('animation-bo')
+    
     pauseAudio(audioOhno)
     audioMeo.play()
+    
     let nap = $("#nap");
     nap.addClass("present-rotate")
-    await sleep(2000)
+    await sleep(audioMeo.duration*1000)
     nap.removeClass("present-rotate")
 });
